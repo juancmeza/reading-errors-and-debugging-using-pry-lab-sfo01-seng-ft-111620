e6a02eb8e3241cf75832b3fc40720805fcede9a0
searching-block-4535 <juancmezat@gmail.com>
2020-10-26 09:23:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,12 @@
 require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    n = 1 
+    while n < 11 do 
+      string = "s" + string
+      n += 1 
+      string
+    end
   else
     string
   end
